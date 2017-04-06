3ce6c158ded0f99c8e1c1dbfb4e99e906e67df12
SonyStone <iliaverpa@gmail.com>
2017-04-06 21:02:24 +0300

    excepthook

--- excepthandling.py
+++ excepthandling.py
@@ -1,5 +1,5 @@
 import maya.utils
 
-def exepthook(tb_type, exc_object, tb, detail=2):
+def excepthook(tb_type, exc_object, tb, detail=2):
 	return 'Hello!'
 maya.utils.formatGuiException = excepthook
