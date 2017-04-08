b7f9f66818146e73e72728133972b77994fdaaad
SonyStone <iliaverpa@gmail.com>
2017-04-08 20:01:48 +0300

    excepthanding.py

--- excepthandling.py
+++ excepthandling.py
@@ -1,5 +1,18 @@
 import maya.utils
 
 def excepthook(tb_type, exc_object, tb, detail=2):
-	return 'Hello!'
+    s = maya.utils._formatGuiException(etype, evalue, tb, detail)
+    lines = [
+        s,
+        'An unhandled exception occurred.',
+        'Please copy the error info above this nessage',
+        'and a copy of this file and email it to',
+        'mayasupport@robg3d.com. You should get a response',
+        'in three days or less.'
+    ]
+    return '\n'.join(lines)
+
 maya.utils.formatGuiException = excepthook
+
+
+
